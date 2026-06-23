118070a3aa7b651e995f231231214305d91f7b8a
Allan Costa <allaninocencio@yahoo.com.br>
2026-06-23 16:25:58 -0300

    chore: remove temporary development files, logs, and utility scripts

--- get_image.py
+++
@@ -1,8 +0,0 @@
-import sys
-from PIL import Image
-
-def main():
-    if len(sys.argv) < 2:
-        return
-    img = Image.open(sys.argv[1])
-    # The actual goal is to look at it, but if view_image fails, we will write a script to display the structure
